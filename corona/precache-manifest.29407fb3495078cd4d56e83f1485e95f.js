81525513a3d54f664513a948dcf202025cd47510
Subramanya HS <subramanya.hs@checkpt.com>
2020-03-27 01:46:21 +0530

    corona stat test app

---
+++ corona/precache-manifest.29407fb3495078cd4d56e83f1485e95f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99065a93b9e855de034d0070d5f57249",
+    "url": "/index.html"
+  },
+  {
+    "revision": "968288126be2288e1535",
+    "url": "/static/css/main.25ac248d.chunk.css"
+  },
+  {
+    "revision": "ce1cd7edb3f263051a7e",
+    "url": "/static/js/2.37c8ae38.chunk.js"
+  },
+  {
+    "revision": "3132d868b2ec05e01cd427559fa94588",
+    "url": "/static/js/2.37c8ae38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "968288126be2288e1535",
+    "url": "/static/js/main.6a0939a2.chunk.js"
+  },
+  {
+    "revision": "34389173c53212cffc2e",
+    "url": "/static/js/runtime-main.691f4b58.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "208f3d38b50b2147d8c20291babed81a",
+    "url": "/static/media/shape.208f3d38.svg"
+  }
+]);
