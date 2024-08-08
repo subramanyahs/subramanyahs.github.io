c280b201ac45062498c7aa038000ff57db391e51
Subramanya HS <subramanyahs@yahoo.com>
2024-08-08 14:26:53 +0530

    Update precache-manifest.5e45dbd3f9168f927d287aee8b7b06ec.js

--- corona_stats/precache-manifest.5e45dbd3f9168f927d287aee8b7b06ec.js
+++ corona_stats/precache-manifest.5e45dbd3f9168f927d287aee8b7b06ec.js
@@ -1,34 +1,34 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "2d1e9d5c99a8778b3e96fe3e63221407",
-    "url": "/index.html"
+    "url": "/corona_stats/index.html"
   },
   {
     "revision": "185986efa2069e1886d7",
-    "url": "/static/css/main.25ac248d.chunk.css"
+    "url": "/corona_stats/static/css/main.25ac248d.chunk.css"
   },
   {
     "revision": "4e936d9e1f435310c149",
-    "url": "/static/js/2.8e512e25.chunk.js"
+    "url": "/corona_stats/static/js/2.8e512e25.chunk.js"
   },
   {
     "revision": "3132d868b2ec05e01cd427559fa94588",
-    "url": "/static/js/2.8e512e25.chunk.js.LICENSE.txt"
+    "url": "/corona_stats/static/js/2.8e512e25.chunk.js.LICENSE.txt"
   },
   {
     "revision": "185986efa2069e1886d7",
-    "url": "/static/js/main.dbf34ca7.chunk.js"
+    "url": "/corona_stats/static/js/main.dbf34ca7.chunk.js"
   },
   {
     "revision": "34389173c53212cffc2e",
-    "url": "/static/js/runtime-main.691f4b58.js"
+    "url": "/corona_stats/static/js/runtime-main.691f4b58.js"
   },
   {
     "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
-    "url": "/static/media/logo.5d5d9eef.svg"
+    "url": "/corona_stats/static/media/logo.5d5d9eef.svg"
   },
   {
     "revision": "208f3d38b50b2147d8c20291babed81a",
-    "url": "/static/media/shape.208f3d38.svg"
+    "url": "/corona_stats/static/media/shape.208f3d38.svg"
   }
 ]);
