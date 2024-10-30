3a37ffb0e68064cf3da36d53ae2f5e6130c1f107
Subramanya H S <subramanyahs@yahoo.com>
2024-10-30 11:33:06 +0530

    aqi code 1

--- aqi/precache-manifest.9cafaa56f9ad584889c7e7ae6cd67b32.js
+++ aqi/precache-manifest.9cafaa56f9ad584889c7e7ae6cd67b32.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "648f060ce6ff29cb74d78fc6c4ea7002",
-    "url": "/index.html"
+    "url": "/aqi/index.html"
   },
   {
     "revision": "b44f2289f477fd889f0d",
-    "url": "/static/css/main.471fbfb1.chunk.css"
+    "url": "/aqi/static/css/main.471fbfb1.chunk.css"
   },
   {
     "revision": "e3793010da9b7ebdaba3",
-    "url": "/static/js/2.5d478d52.chunk.js"
+    "url": "/aqi/static/js/2.5d478d52.chunk.js"
   },
   {
     "revision": "b44f2289f477fd889f0d",
-    "url": "/static/js/main.1be26ce9.chunk.js"
+    "url": "/aqi/static/js/main.1be26ce9.chunk.js"
   },
   {
     "revision": "0d02ce80bdbb08301726",
-    "url": "/static/js/runtime-main.28d06ae5.js"
+    "url": "/aqi/static/js/runtime-main.28d06ae5.js"
   },
   {
     "revision": "17610ce79f101daee426c7c961224dae",
-    "url": "/static/media/search-icon.17610ce7.bin"
+    "url": "/aqi/static/media/search-icon.17610ce7.bin"
   }
 ]);
--- aqi/service-worker.js
+++ aqi/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9cafaa56f9ad584889c7e7ae6cd67b32.js"
+  "/aqi/precache-manifest.9cafaa56f9ad584889c7e7ae6cd67b32.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aqi/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
